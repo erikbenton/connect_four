97dbf2794396d28aa94adeac5e15f1b415e2aaa6
erikbenton <erik.benton@hotmail.com>
2017-12-13 11:20:26 -0500

    laying down framework for tests

---
+++ lib/connect_four.rb
@@ -0,0 +1,33 @@
+class ConnectFour
+	
+	def initialize
+
+
+	end
+
+	def write_instructions
+
+
+	end
+
+
+	def draw_board
+
+
+	end
+
+
+	def make_move(coord)
+
+
+	end
+
+
+	def check_for_win
+
+
+
+	end
+
+
+end
---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,25 @@
+require "connect_four.rb"
+
+
+describe "ConnectFour" do
+
+	describe ".initialize" do
+
+	end
+
+	describe ".write_instructions" do
+
+
+	end
+
+	describe ".draw_board" do
+
+
+	end
+
+	describe ".make_move" do
+
+
+	end
+		
+end
