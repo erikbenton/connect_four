827652954ffdfebbfcffd8faa16c1b50b4119db4
erikbenton <erik.benton@hotmail.com>
2017-12-13 21:08:44 -0500

    fixing dimensions (7x6) -> (6x7) and adding tests for drawing game board

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,25 +1,68 @@
 class ConnectFour
-	
-	def initialize
+	attr_accessor :board
 
+
+	def initialize
+		puts self.write_instructions
+		@board = self.make_board
 
 	end
 
 	def write_instructions
+		instructions = ""
 
-
+		instructions = "Hello, and welcome to ConnectFour!\n"
+		instructions += "The rules are simple:\n"
+		instructions += "Choose a column to drop your piece down\n"
+		instructions += "The piece will go down as far as it can\n"
+		instructions += "Do your best to make it connect 4 pieces in:\n"
+		instructions += "  A row\n"
+		instructions += "  A column\n"
+		instructions += "  Or diagonally\n"
+		instructions += "And you win!\n"
+		instructions += "Goodluck =)\n"
+		return instructions
 	end
 
+	def check_coord(coord)
+		if coord >= 0 && coord < 6
+			return true
+		else
+			return false
+		end
+	end
 
-	def draw_board
+	def check_if_free(coord)
+		return @board[5][coord] == "."
+	end
 
-
+	def make_board
+		board = Hash[(0..5).map { |row| [row, [".",".",".",".",".",".","."]]}]
+		return board
 	end
 
 
 	def make_move(coord)
+		i = 0
+		while i < 6
+			if @board[i][coord] == "."
+				@board[i][coord] == "X"
+				return [i, coord]
+			end
+			i += 1
+		end
+		return false
 
+	end
 
+	def draw_board
+		board = ""
+		i = 5
+		while i >= 0
+			board += @board[i].join(" ") + "\n"
+			i -= 1
+		end
+		return board
 	end
 
 
@@ -31,3 +74,7 @@ class ConnectFour
 
 
 end
+
+game = ConnectFour.new
+
+puts game.draw_board
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -3,22 +3,89 @@ require "connect_four.rb"
 
 describe "ConnectFour" do
 
-	describe ".initialize" do
+	subject(:game) {ConnectFour.new()}
 
+	let(:empty_board) {Hash[(0..5).map { |row| [row, [".",".",".",".",".",".","."]]}]}
+	let(:full_board) {Hash[(0..5).map { |row| [row, ["O","O","O","O","O","O","O"]]}]}
+	let(:empty_board_string) {". . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n. . . . . . .\n"}
+
+	describe ".initialize" do
+		it "it sets up an empty board" do
+			expect(game.board).to eql(empty_board)
+		end
 	end
 
 	describe ".write_instructions" do
-
-
+		it "writes instructions for the game" do
+			expect(game.write_instructions).to be_is_a(String)
+		end
 	end
 
-	describe ".draw_board" do
+	describe ".check_coord" do
+		context "given a valid coordinate" do
+			it "returns true" do
+				expect(game.check_coord(3)).to eql(true)
+			end
+		end
+		context "given an invalid, positive coordinate" do
+			it "returns false" do
+				expect(game.check_coord(10)).to eql(false)
+			end
+		end
+		context "given an invalid, negative coordinate" do
+			it "returns false" do
+				expect(game.check_coord(-5)).to eql(false)
+			end
+		end
+	end
 
-
+	describe ".check_if_free" do
+		context "with an empty board" do
+			it "returns true" do
+				game.board = empty_board
+				expect(game.check_if_free(3)).to eql(true)
+			end
+		end
+		context "with a full column" do
+			it "returns false" do
+				game.board = Hash[(0..5).map { |row| [row, ["O","","","","",""]]}]
+				expect(game.check_if_free(0)).to eql(false)
+			end
+		end
 	end
 
 	describe ".make_move" do
+		context "add piece to empty board" do
+			it "returns the position of the piece" do
+				game.board = empty_board
+				expect(game.make_move(4)).to eql([0,4])
+			end
+		end
+		context "add piece to non-empty board with empty space" do
+			it "returns the position of the piece" do
+				game.board[0] = ["O", "O", ".", ".", "O", ".", "."]
+				game.board[1] = ["O", "O", ".", ".", "O", ".", "."]
+				game.board[2] = ["O", "O", ".", ".", "O", ".", "."]
+				game.board[3] = [".", "O", ".", ".", ".", ".", "."]
+				expect(game.make_move(3)).to eql([0,3])
+			end
+		end
+		context "add piece to full board" do
+			it "returns false" do
+				game.board = full_board
+				expect(game.make_move(4)).to eql(false)
+			end
+		end
 
+		describe ".draw_board" do
+			context "given an empty board" do
+				it "returns a string of a empty board" do
+					game.board = empty_board
+					expect(game.draw_board).to eql(empty_board_string)
+				end
+			end
+
+		end
 
 	end
 		
